260f0146c1616fa45d34ebee45cc1703759f7438
aguscandra1058-SMK <agus.candra1058@guru.smk.belajar.id>
2026-04-29 09:17:27 +0700

    User and Role Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12341234'),
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
